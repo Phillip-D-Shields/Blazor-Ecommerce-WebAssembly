96361143517a578a4430821fef16e2080f253973
phillip shields <matua.phillip.shields@gmail.com>
2023-10-21 16:50:23 +1300

    add product service and interface

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -7,19 +7,19 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IProductService _productService;
 
-        public ProductController(DataContext context)
+        public ProductController(IProductService productService)
         {
-            _context = context;
+               _productService = productService;
         }
 
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
         }
     }
 }
