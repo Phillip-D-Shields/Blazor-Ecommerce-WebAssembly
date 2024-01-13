a01d15848cd0146b334bfd0cecf2f54e8da98842
phillip shields <matua.phillip.shields@gmail.com>
2024-01-13 18:07:19 +1300

    display price or lowest variant price

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -8,6 +8,6 @@
         public string ImageUrl { get; set; } = string.Empty;
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
-        public List<ProductVariant> ProductVariants { get; set; } = new List<ProductVariant>();
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
